d8f971960877257b4738264a7454fa3a384338ff
AnatolyNik <a_nik@hotmail.com>
2025-04-07 17:47:38 +0300

    данные перенесены в data_txt

--- main.py
+++ main.py
@@ -10,18 +10,9 @@ from kivy.uix.boxlayout import BoxLayout
 import random
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
+from data_txt import DATA
 
 kivy.require('2.0.0')
-
-# База данных для карточек
-DATA = {
-    "текст": [("кот", "cat"), ("собака", "dog"), ("птица", "bird"), ("рыба", "fish"), ("лошадь", "horse"),
-              ("дом", "house"), ("книга", "book"), ("солнце", "sun"), ("луна", "moon"), ("звезда", "star")],
-    "картинки": ["image1.png", "image2.png", "image3.png", "image4.png", "image5.png",
-                 "image6.png", "image7.png", "image8.png", "image9.png", "image10.png"],
-    "звуки": ["sound1.mp3", "sound2.mp3", "sound3.mp3", "sound4.mp3", "sound5.mp3",
-              "sound6.mp3", "sound7.mp3", "sound8.mp3", "sound9.mp3", "sound10.mp3"]
-}
 
 class Card(Button):
     card_id = StringProperty(None)
---
+++ test.py
@@ -0,0 +1,2 @@
+from data_txt import DATA
+print("Пример данных:", DATA["текст"][0])
